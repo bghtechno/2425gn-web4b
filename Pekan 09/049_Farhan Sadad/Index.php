58e1fefad623cd807c962e45ff548919fc811e49
farhansadad <sadadfarhan155@gmail.com>
2025-05-22 12:45:39 +0700

    Create Index.php

---
+++ Pekan 09/049_Farhan Sadad/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2>Informasi Waktu</h2>";
+
+echo "<p>Tanggal hari ini: " . date('d-m-Y') . "</p>";
+
+echo "<p>Waktu sekarang: " . date('H:i:s') . "</p>";
+
+echo "<p>Hari: " . date('l') . "</p>";
+
+
+
+?>
